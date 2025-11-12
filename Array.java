507723480299a9ca236b0c3f6f45a30d8eaa4e3c
aravindakuthota <aravindakuthota543@gmail.com>
2025-11-12 23:02:26 +0530

    creating and printing array

---
+++ Array.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class Array {
+    
+public static void main(String[] args) {
+    int nums[] = new int[4]; 
+  
+    for(int i =0; i<4;i++)
+       {
+
+       }
+       
+System.out.println(Arrays.toString(nums));
+
+}
+}
