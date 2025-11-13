eee9936accef38181d757a9a057e48dfc0ac4444
aravindakuthota <aravindakuthota543@gmail.com>
2025-11-13 22:12:24 +0530

    jugged_array_using_random_values

--- jaggedarray.java
+++ jaggedarray.java
@@ -1,16 +1,25 @@
 
 // jagged arrays are defined as which have same type of arrays but different length.
 
+import java.util.Random;
 
 public class jaggedarray {
     public static void main(String[] args) {
-        
-    
+
     int nums[][]  = new int[3][];
 
     nums[0] = new int [3];
     nums[1] = new int[4];
     nums[2]  = new int[5]; 
+
+
+    
+
+    for(int i =0; i<nums.length;i++){
+        for(int j =0; j<nums[i].length;j++){
+            nums[i][j] = (int) (Math.random()*100);
+        }
+    }
 
 
     for(int n[] : nums) {
